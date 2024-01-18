a512fb263bbf4e408b614fdb10a738effcc88623
AleefCma <alefedonato777hotmail.com@gmail.com>
2024-01-18 18:54:33 -0300

    bcrypt.js instalado e implementação na criptografia de senha no DonoModel

--- src/model/DonoModel.js
+++ src/model/DonoModel.js
@@ -1,12 +1,15 @@
 const connection = require('./connection')
+const criptografiaSenha = require('../utils/criptografiaSenha')
 
 const createUser = async (user) => {
 
-    const { Nome, Password,Email } = user
+    const { Nome, Password, Email } = user
+
+    const SenhaCript = await criptografiaSenha.criptografiaSenha(Password.toString())
 
     const query = 'insert into Dono(Nome,Password,Email)values(?,?,?)'
 
-    const createdUser = await connection.execute(query, [Nome,Password,Email ])
+    const createdUser = await connection.execute(query, [Nome, SenhaCript, Email])
 
     return createdUser
 }
@@ -16,13 +19,13 @@ const ValidateUserEmail = async (Email) => {
     const query = 'SELECT * FROM Dono WHERE Email = ?';
 
     const userV = await connection.execute(query, [Email])
-   
+
     return userV
 }
-const loginUser = async (Email,Senha)=>{
-
+const loginUser = async (Email, Senha) => {
+    const SenhaCript = await criptografiaSenha.criptografiaSenha(Senha.toString())
     const query = 'SELECT * FROM Dono WHERE Email = ? AND Password = ?'
-    const userLoggout = await connection.execute(query,[Email,Senha])
+    const userLoggout = await connection.execute(query, [Email, SenhaCript])
 
     return userLoggout
 }
