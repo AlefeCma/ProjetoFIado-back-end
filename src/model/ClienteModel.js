dd3f3325e7a4205ee03abab418a8489f4e43ae93
AleefCma <alefedonato777hotmail.com@gmail.com>
2024-01-26 14:47:31 -0300

    Update ClienteModel.js

--- src/model/ClienteModel.js
+++ src/model/ClienteModel.js
@@ -1,12 +1,17 @@
 const connection = require('./connection')
 
-const cadastroCliente = async (cliente) =>{
-    const {Nome ,DonoId} = cliente
+const cadastroCliente = async (cliente) => {
+    const { Nome, DonoId } = cliente
 
     const query = 'insert into Clientes(Nome , DonoId)values(?,?)'
-    const novoCliente = await connection.execute(query,[Nome,DonoId])
+    const novoCliente = await connection.execute(query, [Nome, DonoId])
 
     return novoCliente
+}
+const listacliente = async () => {
+    const query = 'select * from Cliente'
+    const lista = connection.execute(query)
+    return lista
 }
 module.exports = {
     cadastroCliente
