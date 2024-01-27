36b05a05d1d7e0ea4a27bda357b743b473426ecb
AleefCma <alefedonato777hotmail.com@gmail.com>
2024-01-27 15:12:30 -0300

    Update DividasModel.js

--- src/model/DividasModel.js
+++ src/model/DividasModel.js
@@ -1,1 +1,11 @@
 const connection = require('./connection')
+
+const criarDivida = async (ClienteID, Valor) => {
+    const Data = new Date().toISOString().slice(0, 10);
+    const query = 'INSERT INTO Dividas (ClienteID, Valor, Data) VALUES (?, ?, ?)';
+    const divida = await connection.execute(query, [ClienteID, Valor, Data]);
+    return divida
+}
+module.exports= {
+    criarDivida
+}
